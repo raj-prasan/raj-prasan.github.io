a64bac8e4ac6a2dba3af662144dc5cf29b1b2f41
raj-prasan <rajprasanj@gmail.com>
2025-09-10 06:03:16 +0530

    fixed the overlay problem in mobile devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,8 +46,9 @@ function Navbar() {
                 >{isMenuOpen? <X size ={24}/>:<Menu size ={24}/>}</button>
             <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md x-40 flex flex-col items-center justify-center",
                 "transition-all duration-300 md:hidden",
-                isMenuOpen? "opacity-100 pointer-events-auto": "opacity-0"
+                isMenuOpen? "opacity-100 pointer-events-auto": "opacity-0 pointer-events-none"
             )}>
+                
                 <div className='flex flex-col space-y-8 text-xl'>
                     {navItems.map((item, key) =>(
                         <a key={key} href={item.href} className='md:text-xl text-foreground/80 hover:text-primary transition-colors duration-300'
