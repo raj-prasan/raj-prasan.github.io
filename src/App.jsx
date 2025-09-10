110064fe4290ce71e64d635b9081aa1acf50a1ec
raj-prasan <rajprasanj@gmail.com>
2025-09-10 05:35:02 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound"
 import { Toaster } from "./components/ui/toaster";
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   
 
